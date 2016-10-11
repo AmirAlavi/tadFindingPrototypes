fea49d271ded12c5b531c024c448f6b47e58db44
Amir Alavi <aalavi@andrew.cmu.edu>
2016-10-11 12:36:22 -0400

    Removed experimental code from the repo.

--- boundariesToCoordinates.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = boundariesToCoordinates( boundaries )
-%boundariesToCoordinates Map the boundary data from idea2 onto a coordinate
-% space to see if clustering is a possibility.
-figure()
-scatter(boundaries(:,1), boundaries(:,2))
-end
-
